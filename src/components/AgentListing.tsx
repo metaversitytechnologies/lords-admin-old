85eb698b1b2a9addb3e5095782f1dd8cb54d03fa
ma1hotraz <malhotrazmr@gmail.com>
2025-11-11 12:02:11 +0530

    removed unused inports

--- src/components/AgentListing.tsx
+++ src/components/AgentListing.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const AgentListing = () => {
   return (
     <section className="apl-section">
--- src/components/BalanceInfo.tsx
+++ src/components/BalanceInfo.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const BalanceInfo = () => {
   return (
     <section className="apl-section">
