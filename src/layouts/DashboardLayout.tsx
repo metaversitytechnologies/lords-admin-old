2628f6ef2c4b5360fd84d94d0b7aef52cde7e671
ma1hotraz <malhotrazmr@gmail.com>
2025-11-11 11:57:33 +0530

    Established a DashboardLayout for consistent layout across dashboard pages.

---
+++ src/layouts/DashboardLayout.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Sidebar from "../components/Sidebar";
+import Balance from "../components/Balance";
+import { Outlet } from "react-router-dom";
+
+const DashboardLayout: React.FC = () => {
+  return (
+    <div className="content">
+      <Sidebar />
+      <div className="mid-pane">
+        <Balance />
+        <div className="outlet-container">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardLayout;
