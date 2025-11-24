ff6c8ddf7a1466506ba203c6691fd8f7e5ed0529
ma1hotraz <malhotrazmr@gmail.com>
2025-11-24 18:22:56 +0530

    added proxy

---
+++ src/api/proxy.js
@@ -0,0 +1,23 @@
+export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  const url = "http://150.241.244.175:8070/admin-new-apis" + req.url;
+
+  const response = await fetch(url, {
+    method: req.method,
+    headers: {
+      "Content-Type": "application/json",
+      ...(req.headers.authorization && { Authorization: req.headers.authorization })
+    },
+    body: req.method !== "GET" ? JSON.stringify(req.body) : undefined,
+  });
+
+  const data = await response.json();
+  return res.status(response.status).json(data);
+}
